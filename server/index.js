079538fe6728ad780178ee9126226e60c72d7126
RBK8 <maisaa.alkhedr@gmail.com>
2017-09-24 21:32:38 +0300

    finall push

--- server/index.js
+++ server/index.js
@@ -1,26 +1,63 @@
 var express = require('express');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var useragent = require('express-useragent');
-//var request = require('request');
+var request = require('request');
 var app = express();
 
 //////
 //app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(morgan('dev'));
+app.use(bodyParser.urlencoded());
+app.use(morgan('dev'));
 ///////
+
+
+app.get('/repos', function(req, res){ 
+  var options = {
+  	url: 'https://api.github.com/users/'+req.url.split('?')[1]+'repos',
+    headers: {
+    	'user-agent': 'Maisaa',
+    	'authToken': '6f84a9db63d6450521ba42d46e64a8b8d01a723e'
+    }
+  }
+  request('https://api.github.com', function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred and handle it
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      res.send("hi")
+    });
+
+})
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.post('/repos/import', function (req, res) {
-  // TODO
+  console.log('creating the contacts');
+
+    repo.create({
+        id:req.body.id,
+        name:req.body.name,
+        done: false
+    },function(err,repo){
+        if(err)
+        res.send(err);
+
+        Contact.find({},function(err,repo){
+            if(err)
+            res.send(err);
+            res.json(repo);
+        });
+    });
 
 
 });
 
 app.get('/repos', function (req, res) {
   // TODO
-    res.send(req.useragent);
+  repo.find({},function(err, repos){
+        if(err)
+        res.send(err);
+        res.json(repos);
+    });
+   
 });
 
 var port = 1128;
